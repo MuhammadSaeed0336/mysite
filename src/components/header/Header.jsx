8ca8cc552d2848c7f14cdc568b98136ee1affa52
Saeed <muhammadsaeed033696@gmail.com>
2023-08-24 16:42:35 +0500

    ...

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,7 @@ import "./Header.css";
 import CTA from "./CTA";
 import me from "../../assets/me.jpg";
 import HeaderSocial from "./HeaderSocial";
+import {Link} from "react-router-dom";
 
 function Header() {
 
@@ -11,12 +12,12 @@ function Header() {
       <header id="header">
         <div className="container header_container">
           <h5>Hello I'm</h5>
-          <h1>Muhammad Saeed</h1>
+          <Link to='mailto:muhammadsaeed033696@gmail.com'><h1>Muhammad Saeed</h1></Link>
           <h5 className="text-dark">Front-End Web Developer</h5>
           <CTA />
           <HeaderSocial />
 
-          <div className="me">
+          <div className="me ">
             <img src={me} alt="me" />
           </div>
 
